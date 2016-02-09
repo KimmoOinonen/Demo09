3f0494281fbb7bdb79bbf209b39fba2ad0dd326f
Kimmo Oinonen <kimmo.oinonen94@gmail.com>
2016-02-09 10:54:09 +0200

    Finished.

---
+++ Demo09/T1/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Copyright (C) Kimmo Oinonen
+ *
+ * This file is part of homework project in Object-Oriented
+ * and GUI Programming class in JAMK.
+ *
+ * Created: 9.2.2016
+ * Author: Kimmo Oinonen
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace T1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void trucksButton_Click(object sender, RoutedEventArgs e)
+        {
+            int trucks = int.Parse(trucksTextBlock.Text);
+            trucks++;
+            trucksTextBlock.Text = trucks.ToString();
+        }
+
+        private void carsButton_Click(object sender, RoutedEventArgs e)
+        {
+            int cars = int.Parse(carsTextBlock.Text);
+            cars++;
+            carsTextBlock.Text = cars.ToString();
+        }
+    }
+}
